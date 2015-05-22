014a109d6d7c8a5757931818104108df20762163
Shehzan Mohammed <shehzan@arrayfire.com>
2015-05-22 16:15:13 -0400

    Change freetype-external to freetype-ext
    * Problem when building forge with arrayfire on windows

--- CMakeModules/build_freetype.cmake
+++ CMakeModules/build_freetype.cmake
@@ -3,12 +3,12 @@ INCLUDE(ExternalProject)
 SET(prefix ${CMAKE_BINARY_DIR}/third_party/freetype)
 
 ExternalProject_Add(
-    freetype-external
+    freetype-ext
     GIT_REPOSITORY git://git.sv.nongnu.org/freetype/freetype2.git
     GIT_TAG 14df6b1a63f5c5773bb498063205cb79aac21173
     PREFIX "${prefix}"
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -Wno-dev "-G${CMAKE_GENERATOR}" ${prefix}/src/freetype-external
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -Wno-dev "-G${CMAKE_GENERATOR}" ${prefix}/src/freetype-ext
     -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
     "-DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS} -w -fPIC"
     -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
@@ -20,7 +20,7 @@ ExternalProject_Add(
 ADD_LIBRARY(freetype IMPORTED STATIC)
 SET(freetype_location ${prefix}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}freetype${CMAKE_STATIC_LIBRARY_SUFFIX})
 SET_TARGET_PROPERTIES(freetype PROPERTIES IMPORTED_LOCATION ${freetype_location})
-ADD_DEPENDENCIES(freetype freetype-external)
+ADD_DEPENDENCIES(freetype freetype-ext)
 
 SET(FREETYPE_INCLUDE_DIRS ${prefix}/include/freetype2)
 SET(FREETYPE_LIBRARIES freetype)
