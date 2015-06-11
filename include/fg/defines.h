39852c9bd167157772960aa33eae8608570d9973
pradeep <pradeep@arrayfire.com>
2015-06-11 17:00:12 -0400

    Added documentation
    For the following classes:
    * Window
    * Image
    * Plot
    * Histogram
    * Font

--- include/fg/defines.h
+++ include/fg/defines.h
@@ -37,15 +37,16 @@
 
 #include <GL/glew.h>
 
-// Required to be defined for GLEW MX to work,
-// // along with the GLEW_MX define in the perprocessor!
+/**
+  Requirment by GLEWmx
+ */
 FGAPI GLEWContext* glewGetContext();
 
 namespace fg
 {
 
 enum ErrorCode {
-    FG_SUCCESS            = 0,
+    FG_SUCCESS            = 0,              ///< Fuction returned successfully.
     /*
      * Arguement related error codes that are
      * generated when invalid arguments are
@@ -53,67 +54,72 @@ enum ErrorCode {
      * codes match the following pattern
      * '1***'
      * */
-    FG_ERR_SIZE           = 1001,
-    FG_ERR_INVALID_TYPE   = 1002,
-    FG_ERR_INVALID_ARG    = 1003,
+    FG_ERR_SIZE           = 1001,           ///< Invalid size argument
+    FG_ERR_INVALID_TYPE   = 1002,           ///< Invalid type argument
+    FG_ERR_INVALID_ARG    = 1003,           ///< Invalid argument
     /*
      * OpenGL related error codes
      * match the following pattern
      * '2***'
      * */
-    FG_ERR_GL_ERROR       = 2001,
+    FG_ERR_GL_ERROR       = 2001,           ///< OpenGL error
     /*
      * FreeType related error codes
      * match the following pattern
      * '3***'
      * */
-    FG_ERR_FREETYPE_ERROR = 3001,
+    FG_ERR_FREETYPE_ERROR = 3001,           ///< Freetype library error
     /*
      * File IO related error codes
      * match the following pattern
      * '4***'
      * */
-    FG_ERR_FILE_NOT_FOUND = 4001,
+    FG_ERR_FILE_NOT_FOUND = 4001,           ///< File IO errors
     /*
      * Unsupported configurations
      * and other similar error codes
      * match the following pattern
      * '5***'
      * */
-    FG_ERR_NOT_SUPPORTED  = 5001,
-    FG_ERR_NOT_CONFIGURED = 5002,
+    FG_ERR_NOT_SUPPORTED  = 5001,           ///< Feature not supported
+    FG_ERR_NOT_CONFIGURED = 5002,           ///< Library configuration mismatch
     /*
      * other error codes
      * match the following pattern
      * '9**'
      * */
-    FG_ERR_INTERNAL       = 9001,
-    FG_ERR_RUNTIME        = 9002,
-    FG_ERR_UNKNOWN        = 9003
+    FG_ERR_INTERNAL       = 9001,           ///< Internal error
+    FG_ERR_RUNTIME        = 9002,           ///< Runtime error
+    FG_ERR_UNKNOWN        = 9003            ///< Unkown error
 };
 
 enum ColorMode {
-    FG_RED =1,
-    FG_RGB =3,
-    FG_RGBA=4,
+    FG_RED =1,                              ///< Single channel
+    FG_RGB =3,                              ///< Three(Red, Green & Blue) channels
+    FG_RGBA=4                               ///< Four(Red, Green, Blue & Alpha) channels
 };
 
+/**
+   Color maps
+
+   \image html gfx_palette.png
+ */
 enum ColorMap {
-    FG_DEFAULT  = 0,
-    FG_SPECTRUM = 1,
-    FG_COLORS   = 2,
-    FG_REDMAP   = 3,
-    FG_MOOD     = 4,
-    FG_HEAT     = 5,
-    FG_BLUEMAP  = 6
+    FG_DEFAULT  = 0,                        ///< Default [0-255] grayscale colormap
+    FG_SPECTRUM = 1,                        ///< Spectrum color
+    FG_COLORS   = 2,                        ///< Pure Colors
+    FG_REDMAP   = 3,                        ///< Red color map
+    FG_MOOD     = 4,                        ///< Mood color map
+    FG_HEAT     = 5,                        ///< Heat color map
+    FG_BLUEMAP  = 6                         ///< Blue color map
 };
 
 enum FGType {
-    FG_BYTE         = 0,
-    FG_UNSIGNED_BYTE= 1,
-    FG_INT          = 2,
-    FG_UNSIGNED_INT = 3,
-    FG_FLOAT        = 4,
+    FG_BYTE         = 0,                    ///< Signed byte (8-bits)
+    FG_UNSIGNED_BYTE= 1,                    ///< Unsigned byte (8-bits)
+    FG_INT          = 2,                    ///< Signed integer (32-bits)
+    FG_UNSIGNED_INT = 3,                    ///< Unsigned integer (32-bits)
+    FG_FLOAT        = 4                     ///< Float (32-bits)
 };
 
 }
--- include/fg/font.h
+++ include/fg/font.h
@@ -18,19 +18,54 @@ class _Font;
 namespace fg
 {
 
+/**
+   \class Font
+
+   \brief Font object is essentially a resource handler for the specific font you want to use
+ */
 class Font {
     private:
         internal::_Font* value;
 
     public:
+        /**
+           Creates Font object
+         */
         FGAPI Font();
+
+        /**
+           Copy constructor for Font
+
+           \param[in] other is the Font object of which we make a copy of, this is not a deep copy.
+         */
         FGAPI Font(const Font& other);
+
+        /**
+           Font Destructor
+         */
         FGAPI ~Font();
 
+        /**
+           Load a given font file
+
+           \param[in] pFile True Type Font file path
+           \param[in] pFontSize the size of the font glyphs that will be created
+         */
         FGAPI void loadFont(const char* const pFile, int pFontSize);
+
+        /**
+           Load a system font based on the name
+
+           \param[in] pName True Type Font name
+           \param[in] pFontSize the size of the font glyphs that will be created
+         */
         FGAPI void loadSystemFont(const char* const pName, int pFontSize);
 
         FGAPI void setOthro2D(int pWidth, int pHeight);
+
+        /**
+           Get handle for internal implementation of Font object
+         */
         FGAPI internal::_Font* get() const;
 };
 
