bf5693acb74d4e4b426ef215d91f10aa42833bbe
syurkevi <stefan@arrayfire.com>
2015-09-28 23:24:53 -0400

    plot3d cpu init

--- examples/cpu/plot3d.cpp
+++ examples/cpu/plot3d.cpp
@@ -17,28 +17,40 @@
 const unsigned DIMX = 800;
 const unsigned DIMY = 800;
 
-const float FRANGE_START = 0.f;
-const float FRANGE_END = 2.f * 3.1415926f;
+static const float XMIN = -1.0f;
+static const float XMAX = 2.f;
+static const float YMIN = -1.0f;
+static const float YMAX = 1.f;
 
+static int xsize=0;
+static int ysize=0;
+bool set=false;
 using namespace std;
-void map_range_to_vec_vbo(float range_start, float range_end, float dx, std::vector<float> &vec, float (*map) (float)){
-    if(range_start > range_end && dx > 0) return;
-    for(float i=range_start; i < range_end; i+=dx){
-        vec.push_back(i);
-        vec.push_back((*map)(i));
+void map_range_to_vec_vbo(float dx, std::vector<float> &vec, float (*map) (float), float t){
+    xsize=0;ysize=0;
+    vec.clear();
+    for(float i=XMIN; i < XMAX; i+=dx){
+        xsize++;
+        for(float j=YMIN; j < YMAX; j+=dx){
+            if(!set)ysize++;
+            vec.push_back(i);
+            vec.push_back(j);
+            vec.push_back(10*i*-abs(j) * cos(i*i*(j+t))+sin(j*(i+t))-1.5);
+        }
+        set=true;
     }
 }
 
 int main(void){
     std::vector<float> function;
-    map_range_to_vec_vbo(FRANGE_START, FRANGE_END, 0.1f, function, &sinf);
-
+    static float t=0;
+    map_range_to_vec_vbo(0.01, function, &sinf, t);
     /*
      * First Forge call should be a window creation call
      * so that necessary OpenGL context is created for any
      * other fg::* object to be created successfully
      */
-    fg::Window wnd(DIMX, DIMY, "Plotting Demo");
+    fg::Window wnd(DIMX, DIMY, "3d Surface Demo");
     wnd.makeCurrent();
     /* create an font object and load necessary font
      * and later pass it on to window object so that
@@ -54,17 +66,20 @@ int main(void){
     /* Create several plot objects which creates the necessary
      * vertex buffer objects to hold the different plot types
      */
-    fg::Plot3 plt(10, 10, fg::FG_FLOAT);                              //create a default plot
+    fg::Surface surf(xsize, ysize, fg::FG_FLOAT, fg::FG_SURFACE);
+    fg::Plot plt(xsize*ysize, fg::FG_FLOAT, fg::FG_LINE_3D, fg::FG_NONE);       //or specify a specific plot type
 
     /*
      * Set plot colors
      */
-    plt.setColor(fg::FG_YELLOW);
+    surf.setColor(fg::FG_YELLOW);
+    plt.setColor (fg::FG_WHITE );
 
     /*
      * Set draw limits for plots
      */
-    plt.setAxesLimits(FRANGE_END, FRANGE_START, 1.1f, -1.1f, 1.1f, -1.1f);
+    surf.setAxesLimits(1.1f, -1.1f, 1.1f, -1.1f, 10.f, -5.f);
+    plt.setAxesLimits (1.1f, -1.1f, 1.1f, -1.1f, 10.f, -5.f);
 
     /* copy your data into the pixel buffer object exposed by
      * fg::Plot class and then proceed to rendering.
@@ -72,11 +87,17 @@ int main(void){
      * memory to display memory, Forge provides copy headers
      * along with the library to help with this task
      */
-    copy(plt, &function[0]);
+    copy(surf, &function[0]);
+    copy(plt,  &function[0]);
 
     do {
+        t+=0.01;
+        map_range_to_vec_vbo(0.01f, function, &sinf, t);
+        copy(surf, &function[0]);
+        copy(plt,  &function[0]);
         // draw window and poll for events last
-        wnd.draw(plt);
+        wnd.draw(surf);
+        //wnd.draw(plt);
     } while(!wnd.close());
 
     return 0;
