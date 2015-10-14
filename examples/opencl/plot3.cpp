447c5560ee88aad3b2449ec27c00fe2defe7d656
syurkevi <stefan@arrayfire.com>
2015-10-14 14:08:20 -0400

    adds opencl example

---
+++ examples/opencl/plot3.cpp
@@ -0,0 +1,181 @@
+/*******************************************************
+ * Copyright (c) 2015-2019, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
+#include <forge.h>
+#define __CL_ENABLE_EXCEPTIONS
+#include <cl.hpp>
+#include <OpenCLCopy.hpp>
+#include <mutex>
+#include <complex>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include "cl_helpers.h"
+
+const unsigned DIMX = 1000;
+const unsigned DIMY = 800;
+
+static const float ZMIN = 0.1f;
+static const float ZMAX = 10.f;
+
+const float DX = 0.005;
+static const unsigned ZSIZE = (ZMAX-ZMIN)/DX+1;
+
+
+using namespace std;
+
+static const std::string sincos_surf_kernel =
+"kernel void gen_curve(global float* out, const float t, const float dx, const float zmin, const unsigned SIZE)\n"
+"{\n"
+"    int offset = get_global_id(0);\n"
+"\n"
+"    float z = zmin + offset*dx;\n"
+"    if (offset < SIZE) {\n"
+"       out[offset*3 + 0] = cos(z*t+t)/z;\n"
+"       out[offset*3 + 1] = sin(z*t+t)/z;\n"
+"       out[offset*3 + 2] = z + 0.1*sin(t);\n"
+"    }\n"
+"}\n";
+
+inline int divup(int a, int b)
+{
+    return (a+b-1)/b;
+}
+
+void kernel(cl::Buffer& devOut, cl::CommandQueue& queue, float t)
+{
+    static std::once_flag   compileFlag;
+    static cl::Program      prog;
+    static cl::Kernel       kern;
+
+    std::call_once(compileFlag,
+        [queue]() {
+        prog = cl::Program(queue.getInfo<CL_QUEUE_CONTEXT>(), sincos_surf_kernel, true);
+            kern = cl::Kernel(prog, "gen_curve");
+        });
+
+    NDRange global(ZSIZE);
+
+    kern.setArg(0, devOut);
+    kern.setArg(1, t);
+    kern.setArg(2, DX);
+    kern.setArg(3, ZMIN);
+    kern.setArg(4, ZSIZE);
+    queue.enqueueNDRangeKernel(kern, cl::NullRange, global);
+}
+
+int main(void)
+{
+    try {
+        /*
+         * First Forge call should be a window creation call
+         * so that necessary OpenGL context is created for any
+         * other fg::* object to be created successfully
+         */
+        fg::Window wnd(DIMX, DIMY, "Plot3d Demo");
+        wnd.makeCurrent();
+        /* create an font object and load necessary font
+         * and later pass it on to window object so that
+         * it can be used for rendering text */
+        fg::Font fnt;
+#ifdef OS_WIN
+        fnt.loadSystemFont("Calibri", 32);
+#else
+        fnt.loadSystemFont("Vera", 32);
+#endif
+        wnd.setFont(&fnt);
+
+        /* Create several plot objects which creates the necessary
+         * vertex buffer objects to hold the different plot types
+         */
+        fg::Plot3 plot3(ZSIZE, fg::f32);
+
+        /*
+         * Set draw limits for plots
+         */
+        plot3.setAxesLimits(1.1f, -1.1f, 1.1f, -1.1f, 10.f, 0.f);
+
+        /*
+         * Set draw limits for plots
+         */
+        plot3.setAxesLimits(1.1f, -1.1f, 1.1f, -1.1f, 10.f, 0.f);
+
+        /*
+         * Set axis titles
+         */
+        plot3.setAxesTitles("x-axis", "y-axis", "z-axis");
+
+
+        Platform plat = getPlatform();
+        // Select the default platform and create a context using this platform and the GPU
+#if defined(OS_MAC)
+        CGLContextObj cgl_current_ctx = CGLGetCurrentContext();
+        CGLShareGroupObj cgl_share_group = CGLGetShareGroup(cgl_current_ctx);
+
+        cl_context_properties cps[] = {
+            CL_CONTEXT_PROPERTY_USE_CGL_SHAREGROUP_APPLE, (cl_context_properties)cgl_share_group,
+            0
+        };
+#elif defined(OS_LNX)
+        cl_context_properties cps[] = {
+            CL_GL_CONTEXT_KHR, (cl_context_properties)wnd.context(),
+            CL_GLX_DISPLAY_KHR, (cl_context_properties)wnd.display(),
+            CL_CONTEXT_PLATFORM, (cl_context_properties)plat(),
+            0
+        };
+#else /* OS_WIN */
+        cl_context_properties cps[] = {
+            CL_GL_CONTEXT_KHR, (cl_context_properties)wnd.context(),
+            CL_WGL_HDC_KHR, (cl_context_properties)wnd.display(),
+            CL_CONTEXT_PLATFORM, (cl_context_properties)plat(),
+            0
+        };
+#endif
+        std::vector<Device> devs;
+        plat.getDevices(CL_DEVICE_TYPE_GPU, &devs);
+
+        Device device;
+        for (auto& d : devs) {
+            if (checkExtnAvailability(d, CL_GL_SHARING_EXT)) {
+                device = d;
+                break;
+            }
+        }
+
+        Context context(device, cps);
+        CommandQueue queue(context, device);
+
+        cl::Buffer devOut(context, CL_MEM_READ_WRITE, sizeof(float) * ZSIZE * 3);
+        static float t=0;
+        kernel(devOut, queue, t);
+        /* copy your data into the pixel buffer object exposed by
+         * fg::Surface class and then proceed to rendering.
+         * To help the users with copying the data from compute
+         * memory to display memory, Forge provides copy headers
+         * along with the library to help with this task
+         */
+        fg::copy(plot3, devOut, queue);
+
+        do {
+            t+=0.01;
+            kernel(devOut, queue, t);
+            fg::copy(plot3, devOut, queue);
+            // draw window and poll for events last
+            wnd.draw(plot3);
+        } while(!wnd.close());
+    }catch (fg::Error err) {
+        std::cout << err.what() << "(" << err.err() << ")" << std::endl;
+    } catch (cl::Error err) {
+        std::cout << err.what() << "(" << err.err() << ")" << std::endl;
+    }
+    return 0;
+}
+
