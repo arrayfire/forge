84f82e8f54746d75ddf66eeef0e4368881da1f6e
Shehzan Mohammed <shehzan@arrayfire.com>
2015-12-22 15:21:55 -0500

    Change freetype build directories to ft

--- CMakeModules/build_freetype.cmake
+++ CMakeModules/build_freetype.cmake
@@ -1,6 +1,6 @@
 INCLUDE(ExternalProject)
 
-SET(prefix ${CMAKE_BINARY_DIR}/third_party/freetype)
+SET(prefix ${CMAKE_BINARY_DIR}/third_party/ft)
 SET(freetype_location ${prefix}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}freetype${CMAKE_STATIC_LIBRARY_SUFFIX})
 IF(CMAKE_VERSION VERSION_LESS 3.2)
     IF(CMAKE_GENERATOR MATCHES "Ninja")
@@ -18,12 +18,12 @@ IF(UNIX)
 ENDIF(UNIX)
 
 ExternalProject_Add(
-    freetype-ext
+    ft-ext
     GIT_REPOSITORY http://git.sv.nongnu.org/r/freetype/freetype2.git
     GIT_TAG 14df6b1a63f5c5773bb498063205cb79aac21173
     PREFIX "${prefix}"
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -Wno-dev "-G${CMAKE_GENERATOR}" ${prefix}/src/freetype-ext
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -Wno-dev "-G${CMAKE_GENERATOR}" <SOURCE_DIR>
     -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
     -DCMAKE_CXX_FLAGS:STRING=${CXXFLAGS}
     -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
@@ -36,7 +36,7 @@ ExternalProject_Add(
 ADD_LIBRARY(freetype IMPORTED STATIC)
 SET(freetype_location ${prefix}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}freetype${CMAKE_STATIC_LIBRARY_SUFFIX})
 SET_TARGET_PROPERTIES(freetype PROPERTIES IMPORTED_LOCATION ${freetype_location})
-ADD_DEPENDENCIES(freetype freetype-ext)
+ADD_DEPENDENCIES(freetype ft-ext)
 
 SET(FREETYPE_INCLUDE_DIRS ${prefix}/include/freetype2)
 SET(FREETYPE_LIBRARIES freetype)
