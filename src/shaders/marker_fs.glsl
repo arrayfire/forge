b2d5a6c6ae5748c68ed8f80f47cc8926dddd0b9c
pradeep <pradeep@arrayfire.com>
2016-02-02 00:39:59 +0530

    changes in marker shader equations to draw solid markers

--- src/shaders/marker_fs.glsl
+++ src/shaders/marker_fs.glsl
@@ -10,35 +10,38 @@ out vec4 outColor;
 
 void main(void)
 {
-   float dist = sqrt( (gl_PointCoord.x - 0.5) * (gl_PointCoord.x-0.5) + (gl_PointCoord.y-0.5) * (gl_PointCoord.y-0.5) );
+   vec2 coords = gl_PointCoord;
+   float dist  = sqrt((coords.x - 0.5)*(coords.x-0.5) + (coords.y-0.5)*(coords.y-0.5));
    bool in_bounds;
+
    switch(marker_type) {
        case 1:
-           in_bounds = dist < 0.3;
+           in_bounds = dist<0.1;
            break;
        case 2:
-           in_bounds = ( (dist > 0.3) && (dist<0.5) );
+           in_bounds = dist<0.5;
            break;
        case 3:
-           in_bounds = ((gl_PointCoord.x < 0.15) || (gl_PointCoord.x > 0.85)) ||
-                       ((gl_PointCoord.y < 0.15) || (gl_PointCoord.y > 0.85));
+           in_bounds = ((coords.x > 0.15) || (coords.x < 0.85)) ||
+                       ((coords.y > 0.15) || (coords.y < 0.85));
            break;
        case 4:
-           in_bounds = (2*(gl_PointCoord.x - 0.25) - (gl_PointCoord.y + 0.5) < 0) && (2*(gl_PointCoord.x - 0.25) + (gl_PointCoord.y + 0.5) > 1);
+           in_bounds = (2*(coords.x - 0.25) - (coords.y + 0.5) < 0) &&
+                       (2*(coords.x - 0.25) + (coords.y + 0.5) > 1);
            break;
        case 5:
-           in_bounds = abs((gl_PointCoord.x - 0.5) + (gl_PointCoord.y - 0.5) ) < 0.13  ||
-           abs((gl_PointCoord.x - 0.5) - (gl_PointCoord.y - 0.5) ) < 0.13  ;
+           in_bounds = abs((coords.x - 0.5) + (coords.y - 0.5) ) < 0.13  ||
+                       abs((coords.x - 0.5) - (coords.y - 0.5) ) < 0.13  ;
            break;
        case 6:
-           in_bounds = abs((gl_PointCoord.x - 0.5)) < 0.07 ||
-           abs((gl_PointCoord.y - 0.5)) < 0.07;
+           in_bounds = abs((coords.x - 0.5)) < 0.07 ||
+                       abs((coords.y - 0.5)) < 0.07;
            break;
        case 7:
-           in_bounds = abs((gl_PointCoord.x - 0.5) + (gl_PointCoord.y - 0.5) ) < 0.07 ||
-           abs((gl_PointCoord.x - 0.5) - (gl_PointCoord.y - 0.5) ) < 0.07 ||
-           abs((gl_PointCoord.x - 0.5)) < 0.07 ||
-           abs((gl_PointCoord.y - 0.5)) < 0.07;
+           in_bounds = abs((coords.x - 0.5) + (coords.y - 0.5) ) < 0.07 ||
+                       abs((coords.x - 0.5) - (coords.y - 0.5) ) < 0.07 ||
+                       abs((coords.x - 0.5)) < 0.07 ||
+                       abs((coords.y - 0.5)) < 0.07;
            break;
        default:
            in_bounds = true;
