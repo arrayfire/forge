b4c70b00f97cfcfc6350be05a6192f80116d5a89
pradeep <pradeep@arrayfire.com>
2016-03-04 14:38:35 +0530

    Fixed histogram example for all backends

--- examples/cuda/histogram.cu
+++ examples/cuda/histogram.cu
@@ -15,37 +15,19 @@
 #include <CUDACopy.hpp>
 #include <cstdio>
 
+const unsigned IMGW = 256;
+const unsigned IMGH = 256;
 const unsigned DIMX = 1000;
 const unsigned DIMY = 800;
-const unsigned IMG_SIZE = DIMX * DIMY * 4;
+const unsigned IMG_SIZE = IMGW * IMGH * 4;
 const unsigned WIN_ROWS = 1;
 const unsigned WIN_COLS = 2;
+const unsigned NBINS = 256;
+const float PERSISTENCE = 0.5f;
 
-static float persistance;
-const unsigned NBINS = 5;
-
-const static int hperm[] = {26, 58, 229, 82, 132, 72, 144, 251, 196, 192, 127, 16,
-    68, 118, 104, 213, 91, 105, 203, 61, 59, 93, 136, 249, 27, 137, 141, 223, 119,
-    193, 155, 43, 71, 244, 170, 115, 201, 150, 165, 78, 208, 53, 90, 232, 209, 83,
-    45, 174, 140, 178, 220, 184, 70, 6, 202, 17, 128, 212, 117, 200, 254, 57, 248,
-    62, 164, 172, 19, 177, 241, 103, 48, 38, 210, 129, 23, 211, 8, 112, 107,  126,
-    252,  198, 32, 123, 111,  176,  206, 15, 219, 221, 147, 245, 67, 92, 108, 143,
-    54, 102, 169, 22, 74, 124, 181, 186, 138, 18, 7, 34, 81, 46, 120, 236, 89,228,
-    197, 205, 13, 63, 134,  242, 157, 135, 237, 35, 234, 49, 85, 76, 148, 188, 98,
-    87, 173, 84, 226, 11, 125, 122, 2, 94, 191, 179, 175, 187, 133, 231, 154,  44,
-    28, 110, 247, 121, 146, 240, 97, 88, 130,195, 30, 25, 56, 171, 80, 69, 139, 9,
-    238, 160, 227, 204, 31, 40, 66, 77, 21, 159,  162, 207,  167, 214, 10, 3, 149,
-    194, 239, 166,  145, 235, 20, 50, 113, 189, 99, 37, 86, 42, 168, 114, 96, 246,
-    183, 250, 233, 156, 52,  65, 131, 47,  255, 5, 33, 217, 73, 4, 60, 64, 109, 0,
-    215, 100, 180, 12, 24, 190, 222, 106, 41, 216, 230, 161, 55, 152, 79, 75, 142,
-    36, 101, 1, 253, 225, 51, 224, 182, 116, 218, 95, 39, 158,  14, 243, 151, 163,
-    29, 153, 199, 185
-};
-
-__constant__ int perm[256];
-
-void generateNoisyImage(unsigned char* dev_out);
-void generateHistogram(unsigned char * src, int* hist_out, float* hist_colors);
+void genBaseNoise(float* baseNoise);
+void genPerlinNoise(unsigned char* perlinNoise, float* baseNoise);
+void genHistogram(unsigned char* perlinNoise, int* histOut, float* histColors);
 
 curandState_t* state;
 
@@ -58,13 +40,14 @@ void setupRandomKernel(curandState *states, unsigned long long seed)
 
 int main(void)
 {
-    int *hist_out;
-    float *hist_colors;
-    unsigned char *dev_out;
+    float* baseNoise;
+    unsigned char* perlinNoise;
+
+    int *histOut;
+    float *histColors;
 
     FORGE_CUDA_CHECK(cudaMalloc((void **)&state, NBINS*sizeof(curandState_t)));
     setupRandomKernel <<< 1, NBINS >>> (state, 314567);
-    cudaMemcpyToSymbol(perm, hperm, 256 * sizeof(int));
 
     /*
      * First Forge call should be a window creation call
@@ -79,12 +62,15 @@ int main(void)
      */
     wnd.grid(WIN_ROWS, WIN_COLS);
 
-    fg::Image img(DIMX, DIMY, FG_RGBA, u8);
+    fg::Image img(IMGW, IMGH, FG_RGBA, u8);
 
     fg::Chart chart(FG_2D);
     /* set x axis limits to maximum and minimum values of data
-     * and y axis limits to range [0, nBins]*/
-    chart.setAxesLimits(0, 1, 0, 1000);
+     * and y axis limits to range [0, number of pixels ideally]
+     * but practically total number of pixels as y range will skew
+     * the histogram graph vertically. Therefore setting it to
+     * 25% of total number of pixels */
+    chart.setAxesLimits(0, 1, 0, IMGW*IMGH/(float)(NBINS/4.0));
 
     /*
      * Create histogram object specifying number of bins
@@ -95,171 +81,159 @@ int main(void)
      */
     hist.setColor(FG_YELLOW);
 
-    FORGE_CUDA_CHECK(cudaMalloc((void**)&dev_out, IMG_SIZE));
-    FORGE_CUDA_CHECK(cudaMalloc((void**)&hist_out, NBINS * sizeof(int)));
-    FORGE_CUDA_CHECK(cudaMalloc((void**)&hist_colors, 3*NBINS * sizeof(float)));
+    // IMG_SIZE/4 pixels of each 4 bytes(float)
+    FORGE_CUDA_CHECK(cudaMalloc((void**)&baseNoise, IMG_SIZE));
+    // IMG_SIZE/4 pixels of each 1 bytes(unsigned char)
+    FORGE_CUDA_CHECK(cudaMalloc((void**)&perlinNoise, IMG_SIZE));
+    FORGE_CUDA_CHECK(cudaMalloc((void**)&histOut, NBINS * sizeof(int)));
+    FORGE_CUDA_CHECK(cudaMalloc((void**)&histColors, 3*NBINS * sizeof(float)));
 
-    generateNoisyImage(dev_out);
-    generateHistogram(dev_out, hist_out, hist_colors);
-    fg::copy(img, dev_out);
-    fg::copy(hist.vertices(), hist_out);
-    fg::copy(hist.colors(), hist_colors);
+    genBaseNoise(baseNoise);
+    genPerlinNoise(perlinNoise, baseNoise);
+    genHistogram(perlinNoise, histOut, histColors);
+
+    fg::copy(img, perlinNoise);
+    fg::copy(hist.vertices(), histOut);
+    fg::copy(hist.colors(), histColors);
 
     do {
         wnd.draw(0, 0, img,  "Dynamic Perlin Noise" );
         wnd.draw(1, 0, chart, "Histogram of Noisy Image");
         wnd.swapBuffers();
 
-        generateNoisyImage(dev_out);
-        generateHistogram(dev_out, hist_out, hist_colors);
-        fg::copy(img, dev_out);
         // limit histogram update frequency
-        if(fmod(persistance, 0.5f) < 0.01) {
-            fg::copy(hist.vertices(), hist_out);
-            fg::copy(hist.colors(), hist_colors);
-        }
+        //if(fmod(persistance, 0.5f) < 0.01) {
+            genBaseNoise(baseNoise);
+            genPerlinNoise(perlinNoise, baseNoise);
+            genHistogram(perlinNoise, histOut, histColors);
 
+            fg::copy(img, perlinNoise);
+            fg::copy(hist.vertices(), histOut);
+            fg::copy(hist.colors(), histColors);
+        //}
     } while(!wnd.close());
 
-    FORGE_CUDA_CHECK(cudaFree(dev_out));
-    FORGE_CUDA_CHECK(cudaFree(hist_out));
-    FORGE_CUDA_CHECK(cudaFree(hist_colors));
+    FORGE_CUDA_CHECK(cudaFree(perlinNoise));
+    FORGE_CUDA_CHECK(cudaFree(histOut));
+    FORGE_CUDA_CHECK(cudaFree(histColors));
     return 0;
 }
 
-__device__
-inline float interp(float t)
-{
-    return ((6 * t - 15) * t + 10) * t * t * t;
-}
-
-__device__
-inline float lerp (float x0, float x1, float t)
-{
-        return x0 + (x1 - x0) * t;
-}
-
-__device__
-inline float dot (float2 v0, float2 v1)
-{
-        return v0.x*v1.x + v0.y*v1.y;
-}
-
-__device__
-inline float2 sub (float2 v0, float2 v1)
-{
-        return make_float2(v0.x-v1.x, v0.y-v1.y);
-}
-
-__device__
-float perlinNoise(float x, float y, int tileSize)
-{
-    const float2 default_gradients[] = {
-        make_float2(1,1),
-        make_float2(-1,1),
-        make_float2(1,-1),
-        make_float2(-1,-1)
-    };
-    int x_grid = x/tileSize;
-    int y_grid = y/tileSize;
-    unsigned rand_id0 = perm[(x_grid+2*y_grid) % 256 ] % 4;
-    unsigned rand_id1 = perm[(x_grid+1+2*y_grid) % 256 ] % 4;
-    unsigned rand_id2 = perm[(x_grid+2*(y_grid+1)) % 256 ] % 4;
-    unsigned rand_id3 = perm[(x_grid+1+2*(y_grid+1)) % 256 ] % 4;
-
-    x=fmod(x,__int2float_rd(tileSize))/tileSize;
-    y=fmod(y,__int2float_rd(tileSize))/tileSize;
-    float u = interp(x);
-    float v = interp(y);
-
-    float influence_vecs[4];
-    influence_vecs[0] = dot(sub(make_float2(x,y), make_float2(0,0)), default_gradients[rand_id0]);
-    influence_vecs[1] = dot(sub(make_float2(x,y), make_float2(1,0)), default_gradients[rand_id1]);
-    influence_vecs[2] = dot(sub(make_float2(x,y), make_float2(0,1)), default_gradients[rand_id2]);
-    influence_vecs[3] = dot(sub(make_float2(x,y), make_float2(1,1)),             default_gradients[rand_id3]);
-
-    return lerp(lerp(influence_vecs[0], influence_vecs[1], u), lerp(influence_vecs[2], influence_vecs[3], u), v);
-}
- __device__
-float octavesPerlin(float x, float y, int octaves, float persistence, int tileSize)
-{
-    float total = 0, max_value = 0;
-    float amplitude = 1, frequency = 1;
-    for(int i=0; i<octaves; ++i){
-        total += perlinNoise( x*frequency, y*frequency, tileSize) * amplitude;
-        max_value += amplitude;
-        amplitude *= persistence;
-        frequency *= 2;
-    }
-    return total/max_value;
-}
-
-inline int divup(int a, int b)
+inline
+int divup(int a, int b)
 {
     return (a+b-1)/b;
 }
 
 __global__
-void imageKernel(unsigned char* out, float persistance, int tileSize)
+void baseNoiseKernel(float* baseNoise, curandState* state)
 {
     int x = blockIdx.x * blockDim.x  + threadIdx.x;
     int y = blockIdx.y * blockDim.y  + threadIdx.y;
 
-    if (x<DIMX && y<DIMY) {
-        int offset  = y * DIMX + x;
-        int octaves = 4;
-        int noiseValue    = 255 * octavesPerlin(x, y, octaves, persistance, tileSize);
-        out[offset*4 + 0] = noiseValue;
-        out[offset*4 + 1] = noiseValue;
-        out[offset*4 + 2] = noiseValue;
-        out[offset*4 + 3] = 255;
+    if (x<IMGW && y<IMGH) {
+        baseNoise[y*IMGW+x] = curand_uniform(&state[0]);
     }
 }
 
-void generateNoisyImage(unsigned char* dev_out)
+void genBaseNoise(float* baseNoise)
 {
     static const dim3 threads(8, 8);
-    dim3 blocks(divup(DIMX, threads.x),
-                divup(DIMY, threads.y));
+    dim3 blocks(divup(IMGW, threads.x),
+                divup(IMGH, threads.y));
 
-    static int tileSize = 32; tileSize++;
-    persistance += 0.01;
-    imageKernel<<< blocks, threads >>>(dev_out, persistance, tileSize);
+    baseNoiseKernel<<< blocks, threads >>>(baseNoise, state);
+}
+
+__device__ __inline__
+float interp(float x0, float x1, float t)
+{
+    return x0 + (x1 - x0) * t;
 }
 
 __global__
-void histogramKernel(const unsigned char* src, int* hist_array, const unsigned nbins)
+void perlinKernel(unsigned char* perlinNoise, float* baseNoise)
+{
+    int i = blockIdx.x * blockDim.x  + threadIdx.x;
+    int j = blockIdx.y * blockDim.y  + threadIdx.y;
+
+    if (i<IMGW && j<IMGH) {
+        float pnoise = 0.0f;
+        float amp    = 1.0f;
+        float tamp   = 0.0f;
+
+        for (int octave=6; octave>=0; --octave)
+        {
+            int period = 1 << octave;
+            float freq = 1.0f / period;
+
+            int si0 = (i/period) * period;
+            int si1 = (si0 + period) % IMGW;
+            float hblend = (i - si0) * freq;
+
+            int sj0 = (j/period) * period;
+            int sj1 = (sj0 + period) % IMGH;
+            float vblend = (j - sj0) * freq;
+
+            float top = interp(baseNoise[si0+IMGW*sj0], baseNoise[si1+IMGW*sj0], hblend);
+            float bot = interp(baseNoise[si0+IMGW*sj1], baseNoise[si1+IMGW*sj1], hblend);
+
+            pnoise += (amp * interp(top, bot, vblend));
+
+            tamp += amp;
+            amp *= PERSISTENCE;
+        }
+
+        uint offset = i+j*IMGW;
+        perlinNoise[4*offset+0] = pnoise/tamp;
+        perlinNoise[4*offset+1] = pnoise/tamp;
+        perlinNoise[4*offset+2] = pnoise/tamp;
+        perlinNoise[4*offset+3] = 255;
+    }
+}
+
+void genPerlinNoise(unsigned char* perlinNoise, float* baseNoise)
+{
+    static const dim3 threads(8, 8);
+    dim3 blocks(divup(IMGW, threads.x),
+                divup(IMGH, threads.y));
+
+    perlinKernel<<< blocks, threads >>>(perlinNoise, baseNoise);
+}
+
+__global__
+void histogramKernel(const unsigned char* perlinNoise, int* histOut, const unsigned nbins)
 {
     int x = blockIdx.x * blockDim.x  + threadIdx.x;
     int y = blockIdx.y * blockDim.y  + threadIdx.y;
 
-    if (x<DIMX && y<DIMY) {
-        int offset  = y * DIMX + x;
-        unsigned char noiseVal = src[offset*4 + 0];
+    if (x<IMGW && y<IMGH) {
+        int offset  = y * IMGW + x;
+        unsigned char noiseVal = perlinNoise[offset*4 + 0];
         offset = __float2int_rd(nbins * (__int2float_rd(noiseVal)/255.f));
-        atomicAdd(hist_array + offset , 1);
+        atomicAdd(histOut + offset , 1);
     }
 }
 
 __global__
-void histColorsKernel(float* hist_colors, curandState *states)
+void histColorsKernel(float* histColors, curandState *states)
 {
     int bin = blockIdx.x * blockDim.x  + threadIdx.x;
 
-    hist_colors[3*bin+0] = curand_uniform(&states[bin]);
-    hist_colors[3*bin+1] = curand_uniform(&states[bin]);
-    hist_colors[3*bin+2] = curand_uniform(&states[bin]);
+    histColors[3*bin+0] = curand_uniform(&states[bin]);
+    histColors[3*bin+1] = curand_uniform(&states[bin]);
+    histColors[3*bin+2] = curand_uniform(&states[bin]);
 }
 
-void generateHistogram(unsigned char * src, int* hist_out, float* hist_colors)
+void genHistogram(unsigned char * perlinNoise, int* histOut, float* histColors)
 {
     static const dim3 threads(8, 8);
-    dim3 blocks(divup(DIMX, threads.x),
-                divup(DIMY, threads.y));
+    dim3 blocks(divup(IMGW, threads.x),
+                divup(IMGH, threads.y));
 
-    cudaMemset(hist_out, 0, NBINS * sizeof(int));
+    cudaMemset(histOut, 0, NBINS * sizeof(int));
 
-    histogramKernel<<< blocks, threads >>>(src, hist_out, NBINS);
+    histogramKernel<<< blocks, threads >>>(perlinNoise, histOut, NBINS);
 
-    histColorsKernel<<< 1, NBINS >>>(hist_colors, state);
+    histColorsKernel<<< 1, NBINS >>>(histColors, state);
 }
--- examples/opencl/histogram.cpp
+++ examples/opencl/histogram.cpp
@@ -23,124 +23,90 @@
 using namespace cl;
 using namespace std;
 
+const unsigned IMGW = 256;
+const unsigned IMGH = 256;
 const unsigned DIMX = 1000;
 const unsigned DIMY = 800;
-const unsigned IMG_SIZE = DIMX * DIMY * 4;
-
+const unsigned IMG_SIZE = IMGW * IMGH * 4;
 const unsigned WIN_ROWS = 1;
 const unsigned WIN_COLS = 2;
-
-static float persistance = 0.1;
-const unsigned NBINS = 5;
+const unsigned NBINS = 256;
+const float PERSISTENCE = 0.5f;
 
 static const std::string fractal_ocl_kernel =
-"__constant int perm[] = { 26, 58, 229, 82, 132, 72, 144, 251, 196, 192, 127, 16,\n"
-"    68, 118, 104, 213, 91, 105, 203, 61, 59, 93, 136, 249, 27, 137, 141, 223, 119,\n"
-"    193, 155, 43, 71, 244, 170, 115, 201, 150, 165, 78, 208, 53, 90, 232, 209, 83,\n"
-"    45, 174, 140, 178, 220, 184, 70, 6, 202, 17, 128, 212, 117, 200, 254, 57, 248,\n"
-"    62, 164, 172, 19, 177, 241, 103, 48, 38, 210, 129, 23, 211, 8, 112, 107,  126,\n"
-"    252,  198, 32, 123, 111,  176,  206, 15, 219, 221, 147, 245, 67, 92, 108, 143,\n"
-"    54, 102, 169, 22, 74, 124, 181, 186, 138, 18, 7, 34, 81, 46, 120, 236, 89,228,\n"
-"    197, 205, 13, 63, 134,  242, 157, 135, 237, 35, 234, 49, 85, 76, 148, 188, 98,\n"
-"    87, 173, 84, 226, 11, 125, 122, 2, 94, 191, 179, 175, 187, 133, 231, 154,  44,\n"
-"    28, 110, 247, 121, 146, 240, 97, 88, 130,195, 30, 25, 56, 171, 80, 69, 139, 9,\n"
-"    238, 160, 227, 204, 31, 40, 66, 77, 21, 159,  162, 207,  167, 214, 10, 3, 149,\n"
-"    194, 239, 166,  145, 235, 20, 50, 113, 189, 99, 37, 86, 42, 168, 114, 96, 246,\n"
-"    183, 250, 233, 156, 52,  65, 131, 47,  255, 5, 33, 217, 73, 4, 60, 64, 109, 0,\n"
-"    215, 100, 180, 12, 24, 190, 222, 106, 41, 216, 230, 161, 55, 152, 79, 75, 142,\n"
-"    36, 101, 1, 253, 225, 51, 224, 182, 116, 218, 95, 39, 158,  14, 243, 151, 163,\n"
-"    29, 153, 199, 185\n"
-"};\n"
-"\n"
-"__constant float2 default_gradients[4] = {(float2) (1,1), (float2)(-1,1), (float2)(1,-1), (float2)(-1,-1) };\n"
-"\n"
-"float interp(float t){\n"
-"    return ((6 * t - 15) * t + 10) * t * t * t;\n"
-"}\n"
-"\n"
-"float lerp (float x0, float x1, float t) {\n"
-"        return x0 + (x1 - x0) * t;\n"
-"}\n"
-"\n"
-"float perlinNoise(float x, float y, int tileSize)\n"
-"{\n"
-"    int x_grid = x/tileSize;\n"
-"    int y_grid = y/tileSize;\n"
-"    unsigned rand_id0 = perm[(x_grid+2*y_grid) % 256 ] % 4;\n"
-"    unsigned rand_id1 = perm[(x_grid+1+2*y_grid) % 256 ] % 4;\n"
-"    unsigned rand_id2 = perm[(x_grid+2*(y_grid+1)) % 256 ] % 4;\n"
-"    unsigned rand_id3 = perm[(x_grid+1+2*(y_grid+1)) % 256 ] % 4;\n"
-"\n"
-"    x=fmod(x,tileSize)/tileSize;\n"
-"    y=fmod(y,tileSize)/tileSize;\n"
-"    float u = interp(x);\n"
-"    float v = interp(y);\n"
-"\n"
-"    float influence_vecs[4];\n"
-"    influence_vecs[0] = dot((float2)(x,y) - (float2)(0,0), default_gradients[rand_id0]);\n"
-"    influence_vecs[1] = dot((float2)(x,y) - (float2)(1,0), default_gradients[rand_id1]);\n"
-"    influence_vecs[2] = dot((float2)(x,y) - (float2)(0,1), default_gradients[rand_id2]);\n"
-"    influence_vecs[3] = dot((float2)(x,y) - (float2)(1,1),             default_gradients[rand_id3]);\n"
-"\n"
-"    return lerp(lerp(influence_vecs[0], influence_vecs[1], u), lerp(influence_vecs[2], influence_vecs[3], u), v);\n"
-"}\n"
-"float octavesPerlin(float x, float y, int octaves, float persistence, int tileSize)\n"
-"{\n"
-"    float total = 0, max_value = 0;\n"
-"    float amplitude = 1, frequency = 1;\n"
-"    for(int i=0; i<octaves; ++i){\n"
-"        total += perlinNoise( x*frequency, y*frequency, tileSize) * amplitude;\n"
-"        max_value += amplitude;\n"
-" \n"
-"        amplitude *= persistence;\n"
-"        frequency *= 2;\n"
-"    }\n"
-"    return total/max_value;\n"
-"}\n"
-"\n"
-"kernel\n"
-"void image_gen(global unsigned char* out, const unsigned w, const unsigned h, float persistance, int tileSize)\n"
-"{\n"
-"    int x = get_global_id(0);\n"
-"    int y = get_global_id(1);\n"
-"\n"
-"    if (x<w && y<h) {\n"
-"        int offset  = y * w + x;\n"
-"        int octaves = 4;\n"
-"        int noiseValue    = 255 * octavesPerlin(x, y, octaves, persistance, tileSize);\n"
-"        out[offset*4 + 0] = noiseValue;\n"
-"        out[offset*4 + 1] = noiseValue;\n"
-"        out[offset*4 + 2] = noiseValue;\n"
-"        out[offset*4 + 3] = 255;\n"
-"    }\n"
-"}\n"
-"\n"
-"kernel\n"
-"void hist_freq(const global unsigned char* out, global int* hist_array, const unsigned w, const unsigned h, const unsigned nbins)\n"
-"{\n"
-"    int x = get_global_id(0);\n"
-"    int y = get_global_id(1);\n"
-"\n"
-"    if (x<w && y<h) {\n"
-"        int offset  = y * w + x;\n"
-"        unsigned char noiseVal = out[offset*4 + 0];\n"
-"        atomic_add(hist_array + convert_int(nbins * convert_float(noiseVal)/255.f) , 1);\n"
-"    }\n"
-"}\n"
-"kernel\n"
-"void zero_buffer(global unsigned int* out, const unsigned size)\n"
-"{\n"
-"    if(get_global_id(0) < size) {\n"
-"        out[get_global_id(0)] = 0;\n"
-"    }\n"
-"}\n"
 "float rand(int x)\n"
 "{\n"
 "    x = (x << 13) ^ x;\n"
 "    return ( 1.0 - ( (x * (x * x * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);\n"
 "}\n"
 "kernel\n"
-"void set_colors(global float* out, uint const seed)\n"
+"void baseNoiseKernel(global float* baseNoise, unsigned IMGW, unsigned IMGH, long long seed)\n"
+"{\n"
+"    int x = get_global_id(0);\n"
+"    int y = get_global_id(1);\n"
+"\n"
+"    if (x<IMGW && y<IMGH) {\n"
+"        baseNoise[y*IMGW+x] = curand_uniform(&state[0]);\n"
+"    }\n"
+"}\n"
+"float interp(float x0, float x1, float t) {\n"
+"        return x0 + (x1 - x0) * t;\n"
+"}\n"
+"kernel\n"
+"void perlinKernel(global unsigned char* perlinNoise, global float* baseNoise,\n"
+"                  unsigned IMGW, unsigned IMGH)\n"
+"{\n"
+"    int i = get_global_id(0);\n"
+"    int j = get_global_id(1);\n"
+"\n"
+"    if (i<IMGW && j<IMGH) {\n"
+"        float pnoise = 0.0f;\n"
+"        float amp    = 1.0f;\n"
+"        float tamp   = 0.0f;\n"
+"\n"
+"        for (int octave=6; octave>=0; --octave)\n"
+"        {\n"
+"            int period = 1 << octave;\n"
+"            float freq = 1.0f / period;\n"
+"\n"
+"            int si0 = (i/period) * period;\n"
+"            int si1 = (si0 + period) % IMGW;\n"
+"            float hblend = (i - si0) * freq;\n"
+"\n"
+"            int sj0 = (j/period) * period;\n"
+"            int sj1 = (sj0 + period) % IMGH;\n"
+"            float vblend = (j - sj0) * freq;\n"
+"\n"
+"            float top = interp(baseNoise[si0+IMGW*sj0], baseNoise[si1+IMGW*sj0], hblend);\n"
+"            float bot = interp(baseNoise[si0+IMGW*sj1], baseNoise[si1+IMGW*sj1], hblend);\n"
+"\n"
+"            pnoise += (amp * interp(top, bot, vblend));\n"
+"\n"
+"            tamp += amp;\n"
+"            amp *= PERSISTENCE;\n"
+"        }\n"
+"\n"
+"        uint offset = i+j*IMGW;\n"
+"        perlinNoise[4*offset+0] = pnoise/tamp;\n"
+"        perlinNoise[4*offset+1] = pnoise/tamp;\n"
+"        perlinNoise[4*offset+2] = pnoise/tamp;\n"
+"        perlinNoise[4*offset+3] = 255;\n"
+"    }\n"
+"}\n"
+"kernel\n"
+"void histogramKernel(const global unsigned char* perlinNoise, global int* histOut, const unsigned w, const unsigned h, const unsigned nbins)\n"
+"{\n"
+"    int x = get_global_id(0);\n"
+"    int y = get_global_id(1);\n"
+"\n"
+"    if (x<w && y<h) {\n"
+"        int offset  = y * w + x;\n"
+"        unsigned char noiseVal = perlinNoise[offset*4 + 0];\n"
+"        atomic_add(histOut + convert_int(nbins * convert_float(noiseVal)/255.f) , 1);\n"
+"    }\n"
+"}\n"
+"kernel\n"
+"void setColorsKernel(global float* out, uint const seed)\n"
 "{\n"
 "    int i = get_global_id(0);\n"
 "    out[3*i+0] = (1+rand(seed * i))/2.0f;\n"
@@ -153,49 +119,48 @@ inline int divup(int a, int b)
     return (a+b-1)/b;
 }
 
-void kernel(cl::Buffer& devOut, cl::Buffer& histOut, cl::Buffer& colors, cl::CommandQueue& queue)
+void kernel(cl::Buffer& baseNoise, cl::Buffer& perlinNoise, cl::Buffer& histOut, cl::Buffer& colors,
+            cl::CommandQueue& queue)
 {
     static std::once_flag   compileFlag;
     static cl::Program      prog;
-    static cl::Kernel       kern_img, kern_hist, kern_zero, kern_colors;
+    static cl::Kernel       genBaseNoise, genPerlinNoise, genHistogram, getHistColors;
+
+    std::srand(std::time(0));
 
     std::call_once(compileFlag,
         [queue]() {
         prog = cl::Program(queue.getInfo<CL_QUEUE_CONTEXT>(), fractal_ocl_kernel, true);
-            kern_img    = cl::Kernel(prog, "image_gen");
-            kern_hist   = cl::Kernel(prog, "hist_freq");
-            kern_zero   = cl::Kernel(prog, "zero_buffer");
-            kern_colors = cl::Kernel(prog, "set_colors");
+            genBaseNoise   = cl::Kernel(prog, "baseNoiseKernel");
+            genPerlinNoise = cl::Kernel(prog, "perlinKernel");
+            genHistogram   = cl::Kernel(prog, "histogramKernel");
+            getHistColors  = cl::Kernel(prog, "setColorsKernel");
         });
 
     static const NDRange local(16, 16);
-    NDRange global(local[0] * divup(DIMX, local[0]),
-                   local[1] * divup(DIMY, local[1]));
+    NDRange global(local[0] * divup(IMGW, local[0]),
+                   local[1] * divup(IMGH, local[1]));
 
-    static int tileSize = 32; tileSize++;
-    persistance += 0.01;
-    kern_img.setArg(0, devOut);
-    kern_img.setArg(1, DIMX);
-    kern_img.setArg(2, DIMY);
-    kern_img.setArg(3, persistance);
-    kern_img.setArg(4, tileSize);
-    queue.enqueueNDRangeKernel(kern_img, cl::NullRange, global, local);
+    genBaseNoise.setArg(0, baseNoise);
+    genBaseNoise.setArg(1, IMGW);
+    genBaseNoise.setArg(2, IMGH);
+    genBaseNoise.setArg(3, std::rand());
+    queue.enqueueNDRangeKernel(genBaseNoise, cl::NullRange, global, local);
+
+    genPerlinNoise.setArg(0, perlinNoise);
+    genPerlinNoise.setArg(1, baseNoise);
+    genPerlinNoise.setArg(2, IMGW);
+    genPerlinNoise.setArg(3, IMGH);
+    queue.enqueueNDRangeKernel(genPerlinNoise, cl::NullRange, global, local);
+
+    genHistogram.setArg(0, histOut);
+    genHistogram.setArg(1, NBINS);
+    queue.enqueueNDRangeKernel(genHistogram, cl::NullRange, global, local);
 
     static const NDRange global_hist(NBINS);
-    kern_zero.setArg(0, histOut);
-    kern_zero.setArg(1, NBINS);
-    queue.enqueueNDRangeKernel(kern_zero, cl::NullRange, global_hist);
-
-    kern_hist.setArg(0, devOut);
-    kern_hist.setArg(1, histOut);
-    kern_hist.setArg(2, DIMX);
-    kern_hist.setArg(3, DIMY);
-    kern_hist.setArg(4, NBINS);
-    queue.enqueueNDRangeKernel(kern_hist, cl::NullRange, global, local);
-
-    kern_colors.setArg(0, colors);
-    kern_colors.setArg(1, std::rand());
-    queue.enqueueNDRangeKernel(kern_colors, cl::NullRange, global_hist);
+    getHistColors.setArg(0, colors);
+    getHistColors.setArg(1, std::rand());
+    queue.enqueueNDRangeKernel(getHistColors, cl::NullRange, global_hist);
 }
 
 int main(void)
@@ -214,12 +179,15 @@ int main(void)
          */
         wnd.grid(WIN_ROWS, WIN_COLS);
 
-        fg::Image img(DIMX, DIMY, FG_RGBA, u8);
+        fg::Image img(IMGW, IMGH, FG_RGBA, u8);
 
         fg::Chart chart(FG_2D);
         /* set x axis limits to maximum and minimum values of data
-         * and y axis limits to range [0, nBins]*/
-        chart.setAxesLimits(0, 1, 0, 1000);
+         * and y axis limits to range [0, number of pixels ideally]
+         * but practically total number of pixels as y range will skew
+         * the histogram graph vertically. Therefore setting it to
+         * 25% of total number of pixels */
+        chart.setAxesLimits(0, 1, 0, IMGW*IMGH/(float)(NBINS/4.0));
 
         /*
          * Create histogram object specifying number of bins
@@ -274,7 +242,8 @@ int main(void)
             }
         }
 
-        cl::Buffer devOut(context, CL_MEM_READ_WRITE, IMG_SIZE);
+        cl::Buffer baseNoise(context, CL_MEM_READ_WRITE, IMG_SIZE);
+        cl::Buffer perlinNoise(context, CL_MEM_READ_WRITE, IMG_SIZE);
         cl::Buffer histOut(context, CL_MEM_READ_WRITE, NBINS * sizeof(int));
         cl::Buffer colors(context, CL_MEM_READ_WRITE, 3 * NBINS * sizeof(float));
 
@@ -282,29 +251,28 @@ int main(void)
          * generate image, and prepare data to pass into
          * Histogram's underlying vertex buffer object
          */
-        kernel(devOut, histOut, colors, queue);
+        kernel(baseNoise, perlinNoise, histOut, colors, queue);
          /* To help the users with copying the data from compute
          * memory to display memory, Forge provides copy headers
          * along with the library to help with this task
          */
-        fg::copy(img, devOut, queue);
+        fg::copy(img, perlinNoise, queue);
         fg::copy(hist.vertices(), hist.verticesSize(), histOut, queue);
         fg::copy(hist.vertices(), hist.verticesSize(), colors, queue);
 
         do {
             wnd.draw(0, 0, img,  "Dynamic Perlin Noise" );
             wnd.draw(1, 0, chart, "Histogram of Noisy Image");
-
             wnd.swapBuffers();
 
-            kernel(devOut, histOut, colors, queue);
-            fg::copy(img, devOut, queue);
-
             // limit histogram update frequency
-            if (fmod(persistance, 0.4f) < 0.02f) {
+            //if (fmod(persistance, 0.4f) < 0.02f) {
+                kernel(baseNoise, perlinNoise, histOut, colors, queue);
+
+                fg::copy(img, perlinNoise, queue);
                 fg::copy(hist.vertices(), hist.verticesSize(), histOut, queue);
                 fg::copy(hist.vertices(), hist.verticesSize(), colors, queue);
-            }
+            //}
 
         } while(!wnd.close());
     }catch (fg::Error err) {
