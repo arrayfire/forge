fa54971232ea0c36d4026eddfe057b58b87ea8a5
pradeep <pradeep@arrayfire.com>
2016-05-06 01:28:20 +0530

    BUGFIX: clEnqueueCopyBuffer fix
    Add event based wait for GL object acquire and release calls.

--- include/OpenCLCopy.hpp
+++ include/OpenCLCopy.hpp
@@ -15,16 +15,19 @@ namespace fg
 
 static void copy(fg::Image& out, const cl::Buffer& in, const cl::CommandQueue& queue)
 {
+    cl::Event ev;
     cl::BufferGL pboMapBuffer(queue.getInfo<CL_QUEUE_CONTEXT>(), CL_MEM_WRITE_ONLY, out.pbo(), NULL);
 
     std::vector<cl::Memory> shared_objects;
     shared_objects.push_back(pboMapBuffer);
 
     glFinish();
-    queue.enqueueAcquireGLObjects(&shared_objects);
+    cl_int res = queue.enqueueAcquireGLObjects(&shared_objects, NULL, &ev);
+    ev.wait();
+
     queue.enqueueCopyBuffer(in, pboMapBuffer, 0, 0, out.size(), NULL, NULL);
+    res = queue.enqueueReleaseGLObjects(&shared_objects, NULL, &ev);
     queue.finish();
-    queue.enqueueReleaseGLObjects(&shared_objects);
 }
 
 /*
@@ -39,16 +42,19 @@ static void copy(fg::Image& out, const cl::Buffer& in, const cl::CommandQueue& q
 template<class Renderable>
 void copy(Renderable& out, const cl::Buffer& in, const cl::CommandQueue& queue)
 {
+    cl::Event ev;
     cl::BufferGL vboMapBuffer(queue.getInfo<CL_QUEUE_CONTEXT>(), CL_MEM_WRITE_ONLY, out.vbo(), NULL);
 
     std::vector<cl::Memory> shared_objects;
     shared_objects.push_back(vboMapBuffer);
 
     glFinish();
-    queue.enqueueAcquireGLObjects(&shared_objects);
+    cl_int res = queue.enqueueAcquireGLObjects(&shared_objects, NULL, &ev);
+    ev.wait();
+
     queue.enqueueCopyBuffer(in, vboMapBuffer, 0, 0, out.size(), NULL, NULL);
+    res = queue.enqueueReleaseGLObjects(&shared_objects, NULL, &ev);
     queue.finish();
-    queue.enqueueReleaseGLObjects(&shared_objects);
 }
 
 }
