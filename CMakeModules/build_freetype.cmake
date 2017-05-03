a8584c1c4ab5421d72651c0c99c74ba7de2a54cd
pradeep <pradeep@arrayfire.com>
2017-05-03 15:59:53 +0530

    change freetype build script to build shared libs
    This also bumps up the freetype version to VER-2-7-1 in
    build_freetype.cmake script.

--- CMakeModules/build_freetype.cmake
+++ CMakeModules/build_freetype.cmake
@@ -1,7 +1,15 @@
 INCLUDE(ExternalProject)
 
 SET(prefix ${PROJECT_BINARY_DIR}/third_party/ft)
-SET(freetype_location ${prefix}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}freetype${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+SET(LIB_POSTFIX "")
+IF (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+    SET(LIB_POSTFIX "d")
+ENDIF()
+
+SET(freetype_location
+    ${prefix}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}freetype${LIB_POSTFIX}${CMAKE_SHARED_LIBRARY_SUFFIX})
+
 IF(CMAKE_VERSION VERSION_LESS 3.2)
     IF(CMAKE_GENERATOR MATCHES "Ninja")
         MESSAGE(WARNING "Building freetype with Ninja has known issues with CMake older than 3.2")
@@ -20,8 +28,9 @@ ENDIF(UNIX)
 ExternalProject_Add(
     ft-ext
     GIT_REPOSITORY http://git.sv.nongnu.org/r/freetype/freetype2.git
-    GIT_TAG 14df6b1a63f5c5773bb498063205cb79aac21173
+    GIT_TAG VER-2-7-1
     PREFIX "${prefix}"
+    INSTALL_DIR "${prefix}"
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${CMAKE_COMMAND} -Wno-dev "-G${CMAKE_GENERATOR}" <SOURCE_DIR>
     -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
@@ -29,15 +38,17 @@ ExternalProject_Add(
     -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
     -DCMAKE_C_FLAGS:STRING=${CFLAGS}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-    -DCMAKE_INSTALL_PREFIX:STRING=${prefix}
+    -DBUILD_SHARED_LIBS:BOOL=ON
+    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
     ${byproducts}
     )
 
-ADD_LIBRARY(freetype IMPORTED STATIC)
-SET(freetype_location ${prefix}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}freetype${CMAKE_STATIC_LIBRARY_SUFFIX})
-SET_TARGET_PROPERTIES(freetype PROPERTIES IMPORTED_LOCATION ${freetype_location})
-ADD_DEPENDENCIES(freetype ft-ext)
+ADD_LIBRARY(freetype IMPORTED SHARED)
 
-SET(FREETYPE_INCLUDE_DIRS ${prefix}/include/freetype2)
-SET(FREETYPE_LIBRARIES freetype)
-SET(FREETYPE_FOUND ON)
+ExternalProject_Get_Property(ft-ext install_dir)
+
+SET_TARGET_PROPERTIES(freetype PROPERTIES IMPORTED_LOCATION ${freetype_location})
+
+SET(FREETYPE_INCLUDE_DIRS "${install_dir}/include/freetype2" CACHE INTERNAL "" FORCE)
+SET(FREETYPE_LIBRARIES ${freetype_location} CACHE INTERNAL "" FORCE)
+SET(FREETYPE_FOUND ON CACHE INTERNAL "" FORCE)
