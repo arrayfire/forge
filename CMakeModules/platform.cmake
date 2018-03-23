a5bbb5d3481f84031d40b291afb1201e8dfe990a
pradeep <pradeep@arrayfire.com>
2018-03-23 09:57:48 +0530

    fix: repalce autoresolve dependencies with manual install

--- CMakeModules/platform.cmake
+++ CMakeModules/platform.cmake
@@ -14,13 +14,3 @@ if(APPLE)
   # CMake searches by default.
   set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};/usr/local/opt")
 endif()
-
-function(get_target_library_path out_path target)
-  set(library_full_name "${CMAKE_SHARED_LIBRARY_PREFIX}${target}${CMAKE_SHARED_LIBRARY_SUFFIX}")
-  set(binary_path "${CMAKE_CURRENT_BINARY_DIR}/${library_full_name}")
-  if (WIN32)
-    set(binary_path "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${library_full_name}")
-  endif ()
-  get_native_path(full_path ${binary_path})
-  set(${out_path} ${full_path} PARENT_SCOPE)
-endfunction()
