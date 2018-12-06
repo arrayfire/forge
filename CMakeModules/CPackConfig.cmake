e81cdea489c13386151a4de5ae47688c93ee191a
pradeep <pradeep@arrayfire.com>
2018-12-06 09:59:39 +0530

    Remove name conflicts if forge is used as submodule
    Also made some pkgcfg_lib variables advanced

--- CMakeModules/CPackConfig.cmake
+++ CMakeModules/CPackConfig.cmake
@@ -9,7 +9,7 @@ cmake_minimum_required(VERSION 3.5)
 
 list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/CMakeModules/nsis")
 
-include(Version)
+include(ForgeVersion)
 include(CPackIFW)
 
 set(VENDOR_NAME "ArrayFire")
