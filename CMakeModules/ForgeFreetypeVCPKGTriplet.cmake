4629a668252324ee48387d5d9b3949550b1b07b6
pradeep <pradeep@arrayfire.com>
2018-12-10 16:59:43 +0530

    Build/publish release binaries from appveyor for tags
    Now, using FreeImage dependency that is directly downloaded from
    freeimage download center. Freeimage package from vcpkg has issues
    with static builds.

---
+++ CMakeModules/ForgeFreetypeVCPKGTriplet.cmake
@@ -0,0 +1,8 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+if(${PORT} MATCHES "freetype|bzip2|libpng|zlib")
+    set(VCPKG_CRT_LINKAGE static)
+    set(VCPKG_LIBRARY_LINKAGE static)
+else()
+    set(VCPKG_CRT_LINKAGE dynamic)
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
