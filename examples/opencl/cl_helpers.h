53b74c7d6131d85ce2970cc65c3ed7200f1660c8
pradeep <pradeep@arrayfire.com>
2021-01-26 10:27:13 +0530

    Fix OpenCL examples warnings for newer OpenCL headers

--- examples/opencl/cl_helpers.h
+++ examples/opencl/cl_helpers.h
@@ -11,6 +11,7 @@
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_TARGET_OPENCL_VERSION 120
 
 #if defined(__GNUC__)
 #pragma GCC diagnostic push
