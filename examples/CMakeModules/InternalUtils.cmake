9505aa3dc594d8cf1ffdc8673b68b708b810cd8c
pradeep <pradeep@arrayfire.com>
2021-04-21 12:58:05 +0530

    Fix CUDA examples LINK keyword
    (cherry picked from commit 3ffee54e6095b234e5f9879dbaad3cc1b2fd5d05)

--- examples/CMakeModules/InternalUtils.cmake
+++ examples/CMakeModules/InternalUtils.cmake
@@ -26,14 +26,3 @@ function(conditional_directory variable directory)
         add_subdirectory(${directory})
     endif()
 endfunction()
-
-add_library(OSCompileFlags INTERFACE)
-
-if (WIN32)
-    target_compile_definitions(OSCompileFlags
-        INTERFACE OS_WIN WIN32_MEAN_AND_LEAN)
-elseif (APPLE)
-    target_compile_definitions(OSCompileFlags INTERFACE OS_MAC)
-else(WIN32)
-    target_compile_definitions(OSCompileFlags INTERFACE OS_LNX)
-endif(WIN32)
